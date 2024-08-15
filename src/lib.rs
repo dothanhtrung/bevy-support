f295b515860985a7dd1a87f67e0974a851febf73
Trung Do <dothanhtrung@pm.me>
2024-08-15 17:15:03 +0700

    Draft

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+// Copyright 2024 Trung Do <dothanhtrung@pm.me>
+
+//! ### Plugin
+//! doc goes here
+
+pub fn show_ui<T>(mut mainmenu: Query<&mut Visibility, With<T>>)
+where
+    T: Component,
+{
+    for mut visibility in mainmenu.iter_mut() {
+        *visibility = Visibility::Visible;
+    }
+}
+
+pub fn hide_ui<T>(mut mainmenu: Query<&mut Visibility, With<T>>)
+where
+    T: Component,
+{
+    for mut visibility in mainmenu.iter_mut() {
+        *visibility = Visibility::Hidden;
+    }
+}
